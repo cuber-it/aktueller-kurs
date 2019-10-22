398a872973b274d666963d51fa877f700021c09a
cuber-it <ulrich.cuber@uc-it.de>
2019-10-22 14:44:48 +0200

    Add files via upload

---
+++ tag-1/tag1-2.py
@@ -0,0 +1,16 @@
+text = "2020-10-22 STATUS: Eine Meldung 4711 mit System 0815"
+
+print(text[0])
+print(text[-1])
+print(text[11:17])
+print(text.split())
+
+
+
+
+
+
+
+
+
+print(",".join([text.split()[i]for i in [2,4,6]]))
