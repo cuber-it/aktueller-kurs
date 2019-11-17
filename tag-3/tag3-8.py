f91cad305f098f234c7a78fe676b66ff0954b797
cuber-it <ulrich.cuber@uc-it.de>
2019-11-17 12:55:25 +0100

    Delete tag3-8.py

--- tag-3/tag3-8.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import json
-import pprint
-
-with urllib.request.urlopen('http://dummy.restapiexample.com/api/v1/employees') as response:
-    data = response.read()
-
-data = json.loads(data)
-pprint.pprint(data)
