1d4637a988129320ddd7c11781f355b84d30885b
ucuber <ulrich.cuber@uc-it.de>
2019-11-19 15:16:58 +0100

    Tag2

--- tag-2/tag2-7.py
+++ tag-2/tag2-7.py
@@ -33,8 +33,20 @@ def extract_nutzdaten(data):
                 result[interface_name]["ipv4"] = [zeile[-2], zeile[-1]]
     return result
 
+def search_by_ip(data, ip):
+    for k, v in data.items():
+        if "ipv4" in v and ip == v["ipv4"][0]:
+            print("{} -> {}".format(k, v))
+
+def user_search(data):
+    eingabe = ""
+    while eingabe != "exit":
+        eingabe = input("ip4: ")
+        search_by_ip(data, eingabe)
 
 if __name__ == "__main__":
     roh_daten = read_data(r"D:\Kurse\python\b-germ-rt-dc-7.txt")
     nutz_daten = extract_nutzdaten(roh_daten)
-    print_data(nutz_daten)
+    #print_data(nutz_daten)
+    #search_by_ip(nutz_daten, "10.242.1.70")
+    user_search(nutz_daten)
