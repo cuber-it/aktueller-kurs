2d8fe592c4842bf18807d12d5476aff436c270da
TN03 <tn@kurs.ucit>
2020-05-27 17:57:48 +0200

    Update in Tag 2

--- teilnehmer/tn03/Tag 2/blackjack.py
+++ teilnehmer/tn03/Tag 2/blackjack.py
@@ -2,6 +2,18 @@ import random
 import os
 import re
 
+eingabe = True
+newcard = True
+counter = 50
+deck=[]
+kartenwert=[] ##Test
+result=[]     ##Test
+wert=[11,10,10,10,10,9,8,7,6,5,4,3,2]
+karten=["Ass","König","Dame","Bube","10","9","8","7","6","5","4","3","2"]
+farben=["Karo","Herz","Pike","Kreuz"]
+x=len(karten)-1
+y=len(farben)-1
+
 def count_points(input):
     farben=["Karo","Herz","Pike","Kreuz"]
     y=len(farben)-1
@@ -14,24 +26,20 @@ def count_points(input):
         while y >= 0:
             y = y - 1
             output = output-input.count(farben[y]+" Ass")*11+input.count(farben[y]+" Ass")*1
-    print(output)
     return output
 
-eingabe = True
-newcard = True
-counter = 50
-dealercards = []
-dealerpoints = 0
-usercards = []
-userpoints = 0
-deck=[]
-kartenwert=[] ##Test
-result=[]     ##Test
-wert=[11,10,10,10,10,9,8,7,6,5,4,3,2]
-karten=["Ass","König","Dame","Bube","10","9","8","7","6","5","4","3","2"]
-farben=["Karo","Herz","Pike","Kreuz"]
-x=len(karten)-1
-y=len(farben)-1
+def restart(eingabe):
+    eingabe = "J" in input()
+
+def loss(counter):
+    counter=counter-1
+    print("Sie haben {} was {} Punkte ergibt. Der Dealer hat mit {} {} Punkte\nSie haben leider verloren\nIhr neuer Kontostand beträgt: {} €\nWollen Sie neu starten? J/N".format(usercards, userpoints, dealercards, dealerpoints,counter))
+    restart(eingabe)
+
+def win(counter):
+    counter=counter+1
+    print("Sie haben {} was {} Punkte ergibt. Der Dealer hat mit {} {} Punkte\nSie haben gewonnen!\nIhr neuer Kontostand beträgt: {} €\nWollen Sie neu starten? J/N".format(usercards, userpoints, dealercards, dealerpoints,counter))
+
 while x >= 0:
     y=len(farben)-1
     while y >= 0:
@@ -39,30 +47,44 @@ while x >= 0:
         deck.append(farben[y]+" "+karten[x])
     x = x -1
 while eingabe:
+    dealercards = []
+    dealerpoints = 0
+    usercards = []
+    userpoints = 0
     dealercards=random.choices(deck, k=2)
     usercards=random.choices(deck, k=2)
     userpoints=count_points(usercards)
     dealerpoints=count_points(dealercards)
-    print("Du hast {} was {} Punkte ergibt. Der Dealer hat mit {} {} Punkte""\n""Wollen Sie noch eine Karte? J/N".format(usercards, userpoints, dealercards, dealerpoints))
+    print("Sie haben {} was {} Punkte ergibt. Der Dealer hat mit {} {} Punkte\nWollen Sie noch eine Karte? J/N".format(usercards, userpoints, dealercards, dealerpoints))
     newcard="J" in input()
     while newcard:
         usercards.append(random.choice(deck))
         userpoints=count_points(usercards)
-        print("Du hast {} was {} Punkte ergibt. Der Dealer hat mit {} {} Punkte".format(usercards, userpoints, dealercards, dealerpoints))
-        newcard="J" in input("Wollen Sie noch eine Karte? J/N ")
+        if userpoints > 21:
+            loss(counter)
+            counter=counter-1
+            print(eingabe)
+            break
+        print("Sie haben {} was {} Punkte ergibt. Der Dealer hat mit {} {} Punkte\nWollen Sie noch eine Karte? J/N".format(usercards, userpoints, dealercards, dealerpoints))
+        newcard="J" in input()
     else:
-        print("Du hast {} was {} Punkte ergibt. Der Dealer hat mit {} {} Punkte".format(usercards, userpoints, dealercards, dealerpoints))
-#    if usernumber > randomnumber:
-#        counter = counter + 1
-#        print("You won 1 €! You now have: "+ str(counter)+" €")
-#    elif usernumber < randomnumber:
-#        counter = counter - 1
-#        print("You lost 1 €! You now have: "+ str(counter)+" €")
-#    else:
-#        print("It is a draw! You stay at: "+ str(counter)+" €")
-#    if counter == 0:
-#        print("You lost all your money. Please try again.")
-#        exit()
-#    else:
-#        print("Do you want to roll again? Y/N")
-#        eingabe = "Y" in input()
+        while dealerpoints < userpoints:
+            dealercards.append(random.choice(deck))
+            dealerpoints=count_points(dealercards)
+            if dealerpoints > 21:
+                win(counter)
+                counter=counter+1
+                break
+            else:
+                print("Der Dealer hat eine neue Karte. {} {} Punkte".format(usercards, userpoints, dealercards, dealerpoints))
+        while dealerpoints <= 21:
+            loss(counter)
+            counter=counter-1
+            break
+        else:
+            win(counter)
+            counter=counter+1
+            break
+        if counter == 0:
+            print("You lost all your money. Please try again.")
+            exit()
