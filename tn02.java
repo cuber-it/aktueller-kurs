cf22e03ac3855f79de060ae24ed0931528d70eb2
tn02 <tn02@training.uc-it.de>
2021-09-15 13:09:22 +0200

    func

---
+++ tn02.java
@@ -0,0 +1,17 @@
+public class tn02 
+{
+    double euklidischesDingsbum() {
+       	if (a == 0) {
+	        return b;
+	    } else {
+	        while (b != 0) {
+		    if (a > b) {
+			    a -= b;
+		    } else {
+			    b -= a;
+		    }
+		}
+	}
+	return a;
+    }
+}
