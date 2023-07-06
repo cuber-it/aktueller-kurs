3b9da8976fd71fedeae082068c924f57f65ab5e4
ucuber <info@uc-it.de>
2023-07-06 12:49:58 +0200

    Tag 6: phonenumbers und regex

--- Tag6/phonenumbers_with_module.py
+++ Tag6/phonenumbers_with_module.py
@@ -1,2 +1,20 @@
 import phonenumbers
-from phonenumbers_list import numbers
+from phonenumbers_list import numbers, din_numbers
+
+def is_valid(number):
+    try:
+        parsed_number = phonenumbers.parse(number, "DE")
+        return phonenumbers.is_valid_number(parsed_number)
+    except phonenumbers.phonenumberutil.NumberParseException:
+        return False
+
+valid = []
+invalid = []
+for number in numbers: # din_numbers:
+    if is_valid(number):
+        valid.append(number)
+    else:
+        invalid.append(number)
+
+print("Valid:  ", valid)
+print("Invalid:", invalid)
--- Tag6/phonenumbers_with_regex.py
+++ Tag6/phonenumbers_with_regex.py
@@ -1,4 +1,26 @@
-import regex
-from phonenumbers_list import numbers
+import re
+from phonenumbers_list import numbers, din_numbers
 
-_
+#rex = re.compile(pattern = r'^(\+49|0)[ ]?([1-9][0-9]{1,5})?[ ]?((\([0-9]{1,6}\))|([0-9]{1,6}))?[ ]?([0-9]{1,4})([ -][0-9]{1,4}){0,3}$')
+
+number_1 = r"((\+\d{2}\s\d{2,4}\s\d{8})" # +49 31 12345678
+number_2 = r"(0\d{3,4}\s\d{5,11}(-\d{2})?)" # 0241 12345-67
+number_3 = r"(0\d{3,4}\s\d\s\d{3,9})" # 01234 0 24233243
+number_4 = r"(0\d{3,4}\s\d{5,11}))" # 0123 12345678901
+number_5 = r"110|112"
+
+rex = re.compile(f"{number_1}|{number_2}|{number_3}|{number_4}|{number_5}")
+
+def is_valid(number):
+    return bool(rex.match(number))
+
+valid = []
+invalid = []
+for number in numbers: # din_numbers:
+    if is_valid(number):
+        valid.append(number)
+    else:
+        invalid.append(number)
+
+print("Valid:  ", valid)
+print("Invalid:", invalid)
