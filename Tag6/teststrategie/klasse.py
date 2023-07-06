9062d7d6391563956fd1ed8b6e95dd0c755eac70
ucuber <info@uc-it.de>
2023-07-06 16:10:56 +0200

    Tag 6 uebung pytest

--- Tag6/teststrategie/klasse.py
+++ Tag6/teststrategie/klasse.py
@@ -1,5 +1,8 @@
 class Calculator:
     def __init__(self, a=0, b=0):
+        self._type_check(a, "invalid input")
+        self._type_check(b, "invalid input")
+
         self._a = a
         self._b = b
         self._e = None
@@ -7,9 +10,54 @@ class Calculator:
     def __str__(self):
         return f"{self._a} {self._b} {self._e}"
 
+    def _type_check(self, value, msg="invalud type"):
+        if not isinstance(value, int):
+            raise TypeError(f"{msg} {value}")
+
+    @property
+    def wert_a(self):
+        return self._a
+
+    @wert_a.setter
+    def wert_a(self, wert):
+        self._type_check(wert, "invalid input")
+        self._a = wert
+
+    @property
+    def wert_b(self):
+        return self._b
+
+    @wert_b.setter
+    def wert_b(self, wert):
+        self._type_check(wert, "invalid input")
+        self._b = wert
+
+    @property # readonly, no setter!
+    def ergebnis(self):
+        return self._e
+
     def add(self):
         self._e = self._a + self._b
         return self
+
+    def sub(self):
+        self._e = self._a - self._b
+        return self
+
+    def mul(self):
+        pass
+
+    def div(self):
+        self._e = int(self._a / self._b)
+        return self
+
+    def modulo(self):
+        pass
+
+    def reset(self):
+        pass
+
+
 
 # TODO:
 # sub, mul div modulo
--- Tag6/teststrategie/pytest_test.py
+++ Tag6/teststrategie/pytest_test.py
@@ -1,16 +1,73 @@
 import pytest
 import klasse
 
-def test_init():
+def test_init_valid():
     calc_1 = klasse.Calculator()
     calc_2 = klasse.Calculator(1,2)
 
     assert str(calc_1) == "0 0 None", f"Mismatch {calc_1}"
     assert str(calc_2) == "1 2 None", f"Mismatch {calc_2}"
 
+def test_init_invalid():
+    with pytest.raises(TypeError):
+        calc = klasse.Calculator(3, "a")
+    with pytest.raises(TypeError):
+        calc = klasse.Calculator("a", 3)
+    with pytest.raises(TypeError):
+        calc = klasse.Calculator("a", "a")
+
 def test_add():
-    calc = klasse.Calculator()
+    calc = klasse.Calculator(3, 5)
     result = calc.add()
 
-    assert str(calc) == "0 0 0", f"Mismatch {calc}"
-    assert str(result) == "0 0 0", f"Mismatch {result}"
+    assert str(calc) == "3 5 8", f"Mismatch {calc}"
+    assert str(result) == "3 5 8", f"Mismatch {result}"
+
+def test_sub():
+    calc = klasse.Calculator(3, 5)
+    result = calc.sub()
+
+    assert str(calc) == "3 5 -2", f"Mismatch {calc}"
+    assert str(result) == "3 5 -2", f"Mismatch {result}"
+
+def test_mul():
+    pass
+
+def test_div():
+    calc = klasse.Calculator(4, 2)
+    
+
+def test_div_by_zero():
+    calc = klasse.Calculator(3, 0)
+    with pytest.raises(ZeroDivisionError):
+        calc.div()
+
+def test_modulo():
+    pass
+
+def test_reset():
+    pass
+
+def test_property_wert_a_valid():
+    calc = klasse.Calculator(3, 5)
+    assert calc.wert_a, 3
+    calc.wert_a = 10
+    assert str(calc), "10 5 None"
+
+def test_property_wert_a_invalid():
+    calc = klasse.Calculator(3, 5)
+
+def test_property_wert_b_valid():
+    calc = klasse.Calculator(3, 5)
+    assert calc.wert_b, 5
+    calc.wert_a = 10
+    assert str(calc), "3 10 None"
+
+def test_property_wert_b_invalid():
+    calc = klasse.Calculator(3, 5)
+
+def test_property_ergebnis_valid():
+    calc = klasse.Calculator(3, 5)
+
+def test_property_ergebnis_invalid():
+    calc = klasse.Calculator(3, 5)
