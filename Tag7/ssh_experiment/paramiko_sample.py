ce2811e6a6d46e93e60e9ba0b9de5bec9831ea95
ucuber <info@uc-it.de>
2023-07-14 14:12:10 +0200

    Tag 7 Beispiele

--- Tag7/ssh_experiment/paramiko_sample.py
+++ Tag7/ssh_experiment/paramiko_sample.py
@@ -4,15 +4,15 @@ import paramiko
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-# Connect to the server
-ssh.connect(hostname='localhost', port=2222, username='paramiko', password='password')
+try:
+    # Connect to the server
+    ssh.connect(hostname='localhost', port=2222, username='paramiko', password='password')
 
-# Execute a command
-stdin, stdout, stderr = ssh.exec_command('ls -l /')
+    # Execute a command
+    stdin, stdout, stderr = ssh.exec_command('ls -l /')
 
-# Print the output of the command
-for line in stdout:
-    print(line.strip('\n'))
-
-# Close the connection
-ssh.close()
+    # Print the output of the command
+    for line in stdout:
+        print(line.strip('\n'))
+finally:
+    ssh.close()
