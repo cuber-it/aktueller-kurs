971cafbd046021cacdfd30900519de5cae9d025e
ucuber <info@uc-it.de>
2023-07-21 10:36:12 +0200

    Tag8 Uebungstemplate

---
+++ Tag8/sqlite_sh.py
@@ -0,0 +1,18 @@
+# Aufurf: python sqlite_sh.py datenbankdateiname
+
+# Annahme der argumente
+# Anpassung der Dateiendung -> datebankdateiname.sqlite
+# Check dass Datei existiert
+
+# connection aufbauen
+
+### Loop bis ".exit" eingegeben
+#
+# Kommando ausführe - Fehler einfangen
+# Bei Erfolg Ausgabe des ERgebnisse: status [und z.B. Datensätze bei SELECT]
+
+# importvorschläge:
+# argparse
+# sqlite3
+
+
