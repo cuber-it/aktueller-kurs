364089f4cd6308445a5ea79408a7e070841c7621
ucuber <info@uc-it.de>
2023-07-28 13:52:54 +0200

    Tag9 Loesungsidee

--- Tag9/Uebung_01/data_analyse.py
+++ Tag9/Uebung_01/data_analyse.py
@@ -1,9 +1,24 @@
 import pandas as pd
+import numpy as np
+
 path = r"E:\Workspaces\Kurse\aktueller-kurs\Material\WA_Fn-UseC_-Telco-Customer-Churn.csv"
 
 
-df = pd.read_csv(path)
-print(df)
+
+def data_repair(df):
+    # Das geht sicher auch in pandas oder numpy, aber wir haben es selbst gemacht, da einfacher :-D
+    daten = df.to_dict(orient="records")
+    for row in daten:
+        try:
+            row["TotalCharges"] = float(row["TotalCharges"])
+        except ValueError:
+            row["TotalCharges"] = int(row["tenure"]) * float(row["MonthlyCharges"])
+    return pd.DataFrame(daten)
+
+def filter_missing_values(df):
+    df = df[df["TotalCharges"] != " "]
+    df["TotalCharges"] = pd.to_numeric(df["TotalCharges"])
+    return df
 
 # Aufgabe:
 # tenuer = Laufzeit, Monthly, Total Charges extrahieren
@@ -12,5 +27,14 @@ print(df)
 # Wo gibt es sie?
 # Was ist die Ursache?
 # Reparieren Sie es, wenn es Probleme gibt
+df = pd.read_csv(path)
 df_kosten = df[["tenure", "MonthlyCharges", "TotalCharges"]]
-print(df_kosten)
+
+print(df_kosten.dtypes)
+print(df_kosten[df_kosten['TotalCharges'] == ' '].index)
+
+df_kosten_1 = filter_missing_values(df_kosten)
+df_kosten_2 = data_repair(df_kosten)
+
+print(len(df_kosten_1))
+print(len(df_kosten_2))
