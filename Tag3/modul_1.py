4adef6b329e73eb081a548407b94ef41f59dcb8f
Ulrich Cuber <ulrich.cuber@uc-it.de>
2025-01-22 13:36:32 +0100

    Delete Tag3 directory

--- Tag3/modul_1.py
+++
@@ -1,4 +0,0 @@
-print("Ich lade!!!")
-MODUL_X = 99
-def machWas():
-    return 2 * 2
--- Tag3/prog.py
+++
@@ -1,9 +0,0 @@
-import kurs
-
-print(dir())
-print(dir(kurs))
-
-print(kurs.funktionA())
-print(kurs.funktionB())
-
-kurs.funktionA()
